874bd639371a621c842bbbb5e68435bc7862832a
matiENA <analistaena@gmail.com>
2026-05-23 07:39:04 -0300

    Add route for integrated trips in dataRoutes.js
    Added a new route for integrated trips for Android.

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getSheetData, writeTestLog } from '../controllers/dataController.js';
+import { getSheetData, writeTestLog, getViajesIntegradosDia } from '../controllers/dataController.js';
 
 const router = Router();
 
-// NUEVA RUTA ESCALABLE: Acepta el ID de la planilla y la pestaña
 router.get('/sheet/:spreadsheetId/:sheetName', getSheetData);
 router.post('/logs', writeTestLog);
+
+// 👇 LA NUEVA RUTA PARA ANDROID
+router.get('/viajes-integrados/:spreadsheetId', getViajesIntegradosDia);
 
 export default router;
